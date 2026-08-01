680b8f59b5dfe8e6b79ade09fb5d113ba6df9e4d
EXODIA05 <alanmexodia@gmail.com>
2026-08-01 21:18:10 +0530

    Time: 3 ms (60.5%) | Memory: 21 MB (93.15%) - LeetCommit

--- Algorithms/136-Single-Number/Single-Number.py
+++ Algorithms/136-Single-Number/Single-Number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        emtpy ={}
-        for i in nums:
-            emtpy[i] = emtpy.get(i,0)+1
-        for key,values in emtpy.items():
-            if values ==1:
-                return key
+        single = 0
+        for num in nums :
+            single^=num
+        return single
