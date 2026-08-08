072a5243d20617965553f2af56b0df0ff20cfb56
EXODIA05 <alanmexodia@gmail.com>
2026-08-08 22:51:50 +0530

    Time: 27 ms (33.9%) | Memory: 19.5 MB (72.15%) - LeetCommit

---
+++ Algorithms/383-Ransom-Note/Ransom-Note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, rn: str, mz: str) -> bool:
+        rnn={}
+        mzz={}
+        for i in rn:
+            rnn[i]=rnn.get(i,0) + 1
+        for j in mz:
+            mzz[j]=mzz.get(j,0) + 1
+        for i in rn:
+            if i not in mzz:
+                return False 
+            if rnn[i]>mzz[i]:
+                return False
+        return True
