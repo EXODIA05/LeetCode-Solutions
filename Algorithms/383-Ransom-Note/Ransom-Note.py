a386dde2df24cb49c8f1393488764ae50f16960d
EXODIA05 <alanmexodia@gmail.com>
2026-08-08 23:13:09 +0530

    Time: 21 ms (57.67%) | Memory: 19.6 MB (72.15%) - LeetCommit

--- Algorithms/383-Ransom-Note/Ransom-Note.py
+++ Algorithms/383-Ransom-Note/Ransom-Note.py
@@ -1,14 +1,12 @@
 class Solution:
     def canConstruct(self, rn: str, mz: str) -> bool:
-        rnn={}
-        mzz={}
+        available= {}
+        for i in mz:
+            available[i]=available.get(i,0)+1
         for i in rn:
-            rnn[i]=rnn.get(i,0) + 1
-        for j in mz:
-            mzz[j]=mzz.get(j,0) + 1
-        for i in rnn:
-            if i not in mzz:
-                return False 
-            if rnn[i]>mzz[i]:
+            
+            if available.get(i,0)==0:
                 return False
+            available[i]-=1
         return True
+         
