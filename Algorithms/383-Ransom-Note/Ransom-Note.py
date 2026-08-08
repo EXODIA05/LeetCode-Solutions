2002576a72f16a990e4dc70032cd42d80a45143d
EXODIA05 <alanmexodia@gmail.com>
2026-08-08 22:53:42 +0530

    Time: 23 ms (54.4%) | Memory: 19.5 MB (94.03%) - LeetCommit

--- Algorithms/383-Ransom-Note/Ransom-Note.py
+++ Algorithms/383-Ransom-Note/Ransom-Note.py
@@ -6,7 +6,7 @@ class Solution:
             rnn[i]=rnn.get(i,0) + 1
         for j in mz:
             mzz[j]=mzz.get(j,0) + 1
-        for i in rn:
+        for i in rnn:
             if i not in mzz:
                 return False 
             if rnn[i]>mzz[i]:
