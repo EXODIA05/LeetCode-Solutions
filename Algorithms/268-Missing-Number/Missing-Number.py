266585e3babbf11406eca220c034b0a27437c6e8
EXODIA05 <alanmexodia@gmail.com>
2026-08-15 22:16:56 +0530

    Time: 1 ms (65.47%) | Memory: 21 MB (6.07%) - LeetCommit

--- Algorithms/268-Missing-Number/Missing-Number.py
+++ Algorithms/268-Missing-Number/Missing-Number.py
@@ -1,10 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        missing = 0 
+        """missing = 0 
         while True:
             if missing not in nums:
                 return missing
-            missing +=1
-
-        
-        
+            missing +=1"""
+        hashmap = set(nums)
+        for i in range(len(nums)+1):
+            if i not in hashmap:
+                return i 
