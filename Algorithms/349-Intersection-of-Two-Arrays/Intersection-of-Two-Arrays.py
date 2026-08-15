a2e6f5e88b4d9c4fb1f6a5c5582c88b85c141860
EXODIA05 <alanmexodia@gmail.com>
2026-08-15 19:49:01 +0530

    Time: 7 ms (21.54%) | Memory: 19.4 MB (44.1%) - LeetCommit

--- Algorithms/349-Intersection-of-Two-Arrays/Intersection-of-Two-Arrays.py
+++ Algorithms/349-Intersection-of-Two-Arrays/Intersection-of-Two-Arrays.py
@@ -1,11 +1,10 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hash_map1={}
         a=[]
-        b = list(set(nums1))
-        for i in (b):
-            flag = False
-            if i in nums2:
-                flag = True
-            if flag:
-                 a.append(i)
+        for i in nums1:
+            hash_map1[i]=hash_map1.get(0,i)+1
+        for key in hash_map1:
+            if key in nums2:
+                a.append(key)
         return a
