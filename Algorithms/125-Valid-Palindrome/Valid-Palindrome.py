818eb3e1ea67809eb1622a88a6bce50c1d33f42f
EXODIA05 <alanmexodia@gmail.com>
2026-08-16 14:24:04 +0530

    Time: 11 ms (31.33%) | Memory: 20.1 MB (31.45%) - LeetCommit

--- Algorithms/125-Valid-Palindrome/Valid-Palindrome.py
+++ Algorithms/125-Valid-Palindrome/Valid-Palindrome.py
@@ -1,8 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = "".join(char for char in s if char.isalnum())
-        s = s.lower().replace(" ", "")
-        if s!=s[::-1]:
-            return False
+        s = s.lower()
+        pos = len(s)-1
+        for i in range(len(s)):
+            if s[i]!=s[pos]:
+                return False
+            pos-=1
         return True
         
