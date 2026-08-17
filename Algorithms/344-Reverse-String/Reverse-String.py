5635d139494a68817cc85a3a58e2fff1a2d7fabf
EXODIA05 <alanmexodia@gmail.com>
2026-08-17 17:13:25 +0530

    Time: 3 ms (45.5%) | Memory: 23.4 MB (45.26%) - LeetCommit

--- Algorithms/344-Reverse-String/Reverse-String.py
+++ Algorithms/344-Reverse-String/Reverse-String.py
@@ -1,9 +1,13 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        pos = len(s)-1
+        """pos = len(s)-1
         i = 0
         while(i<pos):
             s[i],s[pos]=s[pos],s[i]
             i=i+1
             pos=pos-1
-        return None
+        return None"""
+
+        # s.reverse()
+        for i in range(len(s)//2):
+            s[i],s[~i]=s[~i],s[i]
