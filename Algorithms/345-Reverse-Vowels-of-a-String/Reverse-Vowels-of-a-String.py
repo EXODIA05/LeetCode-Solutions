1c533d99403d41643349dea29fe911ce5cfc8c77
EXODIA05 <alanmexodia@gmail.com>
2026-08-21 21:14:14 +0530

    Time: 7 ms (87.97%) | Memory: 20.6 MB (51.89%) - LeetCommit

--- Algorithms/345-Reverse-Vowels-of-a-String/Reverse-Vowels-of-a-String.py
+++ Algorithms/345-Reverse-Vowels-of-a-String/Reverse-Vowels-of-a-String.py
@@ -1,8 +1,8 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = "aeiouAEIOU"
-        reverse_list = []
-        a=""
+        vowels = set("aeiouAEIOU")
+        a = ""
+        """reverse_list = []
         pos = len(reverse_list)-1
         for i in s :
             if i in vowels:
@@ -13,4 +13,18 @@ class Solution:
                 pos-=1
             else:
                 a = a + i 
-        return a
+        return a"""
+        left = 0
+        right = len(s)-1
+        s_list = list(s)
+        while left < right:
+            while left<right and s_list[left] not in vowels:
+                left+=1
+            while left<right and s_list[right] not in vowels:
+                right-=1
+            s_list[left],s_list[right]=s_list[right],s_list[left]
+            left+=1
+            right-=1
+        return "".join(s_list)
+
+                    
